fb3cc3276b0ee5e06a5068910f70c0a394a1fbb5
megatrc <Sh0tnik.19@yandex.ru>
2025-04-01 00:23:01 +0300

    Seventeens

--- AboutMe/static/script.js
+++ AboutMe/static/script.js
@@ -1,14 +1,18 @@
 
-let i = 0;
-async function initData(params) {
-    const response = await fetch("/data")
-    const data = await response.json()
-    const len = data.lenght
-    console.log("dmkl")
-    const rev = document.querySelector(".oih")
+let i = 1;
+const rev = document.querySelector(".oih")
     rev.innerHTML = `
             <button class="sides left"><img class="sidI" src="static/lefts.png" alt=""></button>
             <button class="sides right" ><img class="sidI" src="static/rights.png" alt=""></button>`
+    const btn = document.querySelector(".right")
+    const btn2 = document.querySelector(".left")
+
+async function initData(params) {
+    const response = await fetch("/data")
+    const data = await response.json()
+    const len = data.length
+    console.log("dmkl")
+    
     let cart = document.createElement("div")
     cart.classList.add("rev")
     cart.innerHTML = `
@@ -34,13 +38,12 @@ async function initData(params) {
         `
     rev.appendChild(cart)
     rev.appendChild(cart2)
-    const btn = document.querySelector(".right")
-    const btn2 = document.querySelector(".left")
+
     btn.addEventListener("click", () => {
-        rev.innerHTML = `
-        <button class="sides left"><img class="sidI" src="static/lefts.png" alt=""></button>
-        <button class="sides right" ><img class="sidI" src="static/rights.png" alt=""></button>`
-        console.log(i)
+        while (document.querySelectorAll(".rev").length != 0){
+            rev.removeChild(document.querySelectorAll(".rev")[0])
+        }
+        
         const cart = document.createElement("div")
         cart.classList.add("rev")
         if (i == len - 1) {
@@ -55,7 +58,9 @@ async function initData(params) {
                             </div>
                         </div>
                 `
-            console.log("ssw")
+            console.log("Условие 1")
+            console.log(len)
+            console.log(i)
             let cart2 = document.createElement("div")
             cart2.classList.add("rev")
             cart2.innerHTML = `
@@ -70,6 +75,7 @@ async function initData(params) {
             rev.appendChild(cart)
             rev.appendChild(cart2)
             i = 0
+            console.log(i)
         }
         else{
             let cart = document.createElement("div")
@@ -83,7 +89,10 @@ async function initData(params) {
                             </div>
                         </div>
                 `
-            console.log("ssw")
+            console.log("Условие 2")
+            console.log(len)
+            console.log(i)
+            console.log(i + 1)
             let cart2 = document.createElement("div")
             cart2.classList.add("rev")
             cart2.innerHTML = `
@@ -97,9 +106,113 @@ async function initData(params) {
                 `
             rev.appendChild(cart)
             rev.appendChild(cart2)
-        i++
+            i++
+        }
+
+    })
+    btn2.addEventListener("click", () => {
+        while (document.querySelectorAll(".rev").length != 0){
+            rev.removeChild(document.querySelectorAll(".rev")[0])
+        }
+        const cart = document.createElement("div")
+        cart.classList.add("rev")
+        if (i == 1) {
+            let cart = document.createElement("div")
+            cart.classList.add("rev")
+            cart.innerHTML = `
+                        <div class="revcart">
+                            <p class="revt">${data[i - 1].Text}</p>
+                            <div class="autrev">
+                                <img src="${data[i - 1].Image}" alt="тут аватарка" class="revic">
+                                <p class="revau">${data[i - 1].Author}</p>
+                            </div>
+                        </div>
+                `
+            console.log("Условие 1")
+            console.log(len)
+            console.log(i)
+            let cart2 = document.createElement("div")
+            cart2.classList.add("rev")
+            cart2.innerHTML = `
+                        <div class="revcart">
+                            <p class="revt">${data[len - 1].Text}</p>
+                            <div class="autrev">
+                                <img src="${data[len - 1].Image}" alt="тут аватарка" class="revic">
+                                <p class="revau">${data[len - 1].Author}</p>
+                            </div>
+                        </div>
+                `
+            rev.appendChild(cart2)
+            rev.appendChild(cart)
+            i = len - 1
+            console.log(i)
+        }
+        else if ( i == 0){
+            let cart = document.createElement("div")
+            cart.classList.add("rev")
+            cart.innerHTML = `
+                        <div class="revcart">
+                            <p class="revt">${data[len - 1].Text}</p>
+                            <div class="autrev">
+                                <img src="${data[len - 1].Image}" alt="тут аватарка" class="revic">
+                                <p class="revau">${data[len - 1].Author}</p>
+                            </div>
+                        </div>
+                `
+            console.log("Условие 1")
+            console.log(len)
+            console.log(i)
+            let cart2 = document.createElement("div")
+            cart2.classList.add("rev")
+            cart2.innerHTML = `
+                        <div class="revcart">
+                            <p class="revt">${data[len - 2].Text}</p>
+                            <div class="autrev">
+                                <img src="${data[len - 2].Image}" alt="тут аватарка" class="revic">
+                                <p class="revau">${data[len - 2].Author}</p>
+                            </div>
+                        </div>
+                `
+            rev.appendChild(cart2)
+            rev.appendChild(cart)
+            i = len - 1
+            console.log(i)
+        }
+        else{
+            let cart = document.createElement("div")
+            cart.classList.add("rev")
+            cart.innerHTML = `
+                        <div class="revcart">
+                            <p class="revt">${data[i - 1].Text}</p>
+                            <div class="autrev">
+                                <img src="${data[i - 1].Image}" alt="тут аватарка" class="revic">
+                                <p class="revau">${data[i - 1].Author}</p>
+                            </div>
+                        </div>
+                `
+            console.log("Условие 2")
+            console.log(len)
+            console.log(i)
+            console.log(i - 1)
+            let cart2 = document.createElement("div")
+            cart2.classList.add("rev")
+            cart2.innerHTML = `
+                        <div class="revcart">
+                            <p class="revt">${data[i - 2].Text}</p>
+                            <div class="autrev">
+                                <img src="${data[i - 2].Image}" alt="тут аватарка" class="revic">
+                                <p class="revau">${data[i - 2].Author}</p>
+                            </div>
+                        </div>
+                `
+            rev.appendChild(cart2)
+            rev.appendChild(cart)
+            i--
         }
     })
 }
 initData()
 
+btn.addEventListener("click", () => {
+    console.log("1s")
+})
